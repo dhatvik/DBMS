46ebf8740e5b62b387f3eb06f64a11dafafaac5b
Dhatvik.M.P <75828380+dhatvik@users.noreply.github.com>
2023-01-18 13:49:13 -0500

    Update Exercise3.sql

--- Exercise3.sql
+++ Exercise3.sql
@@ -13,10 +13,70 @@ with the number of boats reserved.
 4. Identify which boats have the same name as their sailor.
 e) Create the table, insert suitable tuples and perform the following operations using
 MongoDB.
+
+db.createCollection('boaters')
+db.boaters.insertMany([
+    {bid:11,bname:'INS',color:'Red',sid:101,sname:'A'},
+    {bid:22,bname:'SOP',color:'Green',sid:102,sname:'B'},
+    {bid:33,bname:'ANS',color:'Black',sid:103,sname:'C'},
+    {bid:44,bname:'IBP',color:'Blue',sid:101,sname:'A'},
+    ])
+    
+db.boaters.find().pretty()
+
+{ "ok" : 1 }
+{
+	"acknowledged" : true,
+	"insertedIds" : [
+		ObjectId("63c83eabc7e159f60bc9d9b0"),
+		ObjectId("63c83eabc7e159f60bc9d9b1"),
+		ObjectId("63c83eabc7e159f60bc9d9b2"),
+		ObjectId("63c83eabc7e159f60bc9d9b3")
+	]
+}
+{
+	"_id" : ObjectId("63c83eabc7e159f60bc9d9b0"),
+	"bid" : 11,
+	"bname" : "INS",
+	"color" : "Red",
+	"sid" : 101,
+	"sname" : "A"
+}
+{
+	"_id" : ObjectId("63c83eabc7e159f60bc9d9b1"),
+	"bid" : 22,
+	"bname" : "SOP",
+	"color" : "Green",
+	"sid" : 102,
+	"sname" : "B"
+}
+{
+	"_id" : ObjectId("63c83eabc7e159f60bc9d9b2"),
+	"bid" : 33,
+	"bname" : "ANS",
+	"color" : "Black",
+	"sid" : 103,
+	"sname" : "C"
+}
+{
+	"_id" : ObjectId("63c83eabc7e159f60bc9d9b3"),
+	"bid" : 44,
+	"bname" : "IBP",
+	"color" : "Blue",
+	"sid" : 101,
+	"sname" : "A"
+}
+
+
 1. Obtain the number of boats obtained by sailor :#Sailor_Name
+
+db.boaters.find({sname:'A'}).count()
+2
+
 2. Retrieve boats of color :”#color”
 
-
+db.boaters.find({color:'Black'})
+{ "_id" : ObjectId("63c83eabc7e159f60bc9d9b2"), "bid" : 33, "bname" : "ANS", "color" : "Black", "sid" : 103, "sname" : "C" }
 F) Write a PL/SQL program to check whether a given number is prime or not
 
 
