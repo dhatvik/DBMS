5b9266ecd54348ef411f3f69378d414d326c97a3
Dhatvik.M.P <75828380+dhatvik@users.noreply.github.com>
2023-01-19 05:34:49 -0500

    Update Exercise3.sql

--- Exercise3.sql
+++ Exercise3.sql
@@ -6,11 +6,116 @@ a) Mention the constraints neatly.
 b) Design the ER diagram for the problem statement
 c) State the schema diagram for the ER diagram.
 d) Create the tables, insert suitable tuples and perform the following operations in SQL:
+
+SQL> create table sailor(
+  2  sid number(3) primary key,
+  3  sname varchar(10) ,
+  4  addr varchar(10));
+
+Table created.
+
+SQL> create table boat(
+  2  bid number(3) primary key,
+  3  bname varchar(10),
+  4  bcolor varchar(10));
+
+Table created.
+
+SQL> create table reserves(
+  2  bid number(3) references boat(bid) on delete cascade,
+  3  sid number(3) references sailor(sid) on delete cascade,
+  4  primary key(bid,sid));
+
+Table created.
+
+SQL> select * from sailor;
+
+       SID SNAME      ADDR
+---------- ---------- ----------
+        11 ram        hunsur
+        22 sham       mysore
+        33 nandan     udupi
+        44 umesh      kerala
+	
+SQL> select * from boat;
+
+       BID BNAME      BCOLOR
+---------- ---------- ----------
+         1 ins        black
+         2 vik        blue
+         3 rant       white
+         4 nandan     red
+	 
+
+SQL> select * from reserves;
+
+       BID        SID
+---------- ----------
+         1         11
+         1         22
+         1         33
+         1         44
+         2         11
+         3         22
+         4         33
+         4         44	 
+	
 1. Obtain the details of the boats reserved by ‘#Sailor_Name’.
+
+SQL> select * from boat
+  2  where bid in (select bid from reserves
+  3  where sid in (select sid from sailor where sname='ram'));
+
+       BID BNAME      BCOLOR
+---------- ---------- ----------
+         1 ins        black
+         2 vik        blue
+	 
+	 
 2. Retrieve the BID of the boats reserved necessarily by all the sailors.
+
+SQL> select *
+  2  from boat
+  3  where not exists(select sid
+  4                   from sailor
+  5                     minus
+  6                   select sid
+  7                  from reserves
+  8                  where boat.bid=reserves.bid);
+
+       BID BNAME      BCOLOR
+---------- ---------- ----------
+         1 ins        black
+	 
+	 
 3. Find the number of boats reserved by each sailor. Display the Sailor_Name along
 with the number of boats reserved.
+
+
+SQL> select sname,count(reserves.bid)
+  2  from sailor,reserves
+  3  where sailor.sid=reserves.sid
+  4  group by sname;
+
+SNAME      COUNT(RESERVES.BID)
+---------- -------------------
+sham                         2
+ram                          2
+nandan                       2
+umesh                        2
+
+
 4. Identify which boats have the same name as their sailor.
+
+SQL> select boat.bid,boat.bname
+  2  from boat,sailor
+  3  where boat.bname=sailor.sname;
+
+       BID BNAME
+---------- ----------
+         4 nandan
+	 
+	 
 e) Create the table, insert suitable tuples and perform the following operations using
 MongoDB.
 
